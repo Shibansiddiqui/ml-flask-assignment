3b6ca9afec3b7ed1cf20c1ac666023cd28b081dc
Shiban Siddiqui <shibansiddiqui@gmail.com>
2026-08-28 09:24:57 +0530

    fix

--- app.py
+++ app.py
@@ -95,10 +95,10 @@ def home():
 #             "error": f"Prediction error: {str(e)}"
 #         }), 500
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(
-#         host="0.0.0.0",
-#         port=port,
-#         debug=False
-#     )
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
